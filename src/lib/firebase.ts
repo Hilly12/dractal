20e59468f23c15f182a9b14314ed0c8c5079c726
Hilly12 <aahilmehta12@gmail.com>
2023-07-02 01:02:39 -0700

    Update firebase config.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,6 @@ import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCyKRPGghIhGckafC_fZOAmXzYxRi82Vzo",
-  authDomain: "dractal-site.firebaseapp.com",
-  projectId: "dractal-site",
-  storageBucket: "dractal-site.appspot.com",
-  messagingSenderId: "864406471527",
-  appId: "1:864406471527:web:4a1a171e285b6122abe5c2",
-  measurementId: "G-1L2EYTSHH8",
 };
 
 // Initialize Firebase
